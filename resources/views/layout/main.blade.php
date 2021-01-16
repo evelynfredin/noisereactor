9ebaa5fcfe06d0337839f880f6d48d52d4afd1d9
Evelyn Fredin <evefre2001@skola.goteborg.se>
2021-01-16 11:34:01 +0100

    Initial project setup

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Noise Reactor</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body class="bg-blue-500">
+    @yield('content')
+</body>
+
+</html>
