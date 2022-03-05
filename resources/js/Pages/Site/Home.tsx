43f70d1cf8ec24bd91b81bc185ba0f703d088d47
Evelyn Fredin <evefre2001@skola.goteborg.se>
2022-03-05 11:23:44 +0100

    Add Main layout. Add Artists page

--- resources/js/Pages/Site/Home.tsx
+++ resources/js/Pages/Site/Home.tsx
@@ -1,12 +1,11 @@
-import Header from '@/Components/Site/Header';
+import Main from '@/Layouts/Main';
 import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <Header />
-      <p>yess</p>
-    </div>
+    <Main title="Home">
+      <p>Hello world</p>
+    </Main>
   );
 };
 
