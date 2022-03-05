e8002a9592b88ce9b8b442185591b49bc039e4b0
Evelyn Fredin <evefre2001@skola.goteborg.se>
2022-03-05 13:44:59 +0100

    Add Album and Label factories/seeders

---
+++ database/factories/AlbumFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Artist;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Album>
+ */
+class AlbumFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->words(rand(1, 3), true),
+            'edition' => 'Standard',
+            'label_id' => $this->faker->numberBetween(rand(1, 6), true),
+            'description' => $this->faker->sentence(4),
+            'released_date' => $this->faker->date($format = 'Y-m-d', $max = 'now')
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Album;
 use App\Models\Artist;
+use App\Models\Label;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,6 +18,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        Artist::factory(12)->create();
+        Label::factory(6)->create();
+        Artist::factory(12)
+            ->has(Album::factory())
+            ->create();
     }
 }
