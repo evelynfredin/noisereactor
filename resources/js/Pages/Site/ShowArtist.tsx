22cf290c74e430f2f29453eccb9ec9b8ffbf9537
Evelyn Fredin <evefre2001@skola.goteborg.se>
2022-03-06 10:55:55 +0100

    Artist page shows artists related genres

--- resources/js/Pages/Site/ShowArtist.tsx
+++ resources/js/Pages/Site/ShowArtist.tsx
@@ -15,7 +15,21 @@ const ShowArtist = ({ artist }: Props) => {
           website={artist.website}
           pic={artist.pic}
           bio={artist.bio}
-        />
+        >
+          {artist.genres.length > 0 && (
+            <div className="border rounded-lg mt-5 p-5">
+              <h3 className="text-xl">Genres</h3>
+              {artist.genres.map((genre) => (
+                <p
+                  key={genre.id}
+                  className="bg-blue-300 inline-block px-3 py-2 rounded-3xl mt-3 mr-2 text-gray-600"
+                >
+                  {genre.name}
+                </p>
+              ))}
+            </div>
+          )}
+        </ArtistInfo>
       </section>
     </Main>
   );
--- resources/js/decls/app.d.ts
+++ resources/js/decls/app.d.ts
@@ -7,7 +7,13 @@ declare namespace App {
     slug: string;
     pic: string;
     albums_count?: number;
+    genres: Genre[];
     created_at: string;
     updated_at: string;
+  }
+
+  interface Genre {
+    id: number;
+    name: string;
   }
 }
