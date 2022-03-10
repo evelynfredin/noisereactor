9d14daa6950e7a8d46d9f626eb5171e3445f4e05
Evelyn Fredin <evefre2001@skola.goteborg.se>
2022-03-10 22:31:56 +0100

    Fix structure of sharedProps and Inertia Request

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Resources\AuthUserResource;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -34,9 +35,17 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            'auth' => [
-                'user' => $request->user(),
-            ],
+            'user' => function () use ($request) {
+                if (!$request->user()) {
+                    return;
+                }
+                return new AuthUserResource($request->user());
+            },
+            'flash' => function () use ($request) {
+                return [
+                    'message' => $request->session()->get('message')
+                ];
+            }
         ]);
     }
 }
