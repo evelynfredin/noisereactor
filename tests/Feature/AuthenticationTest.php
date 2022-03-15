c51e4a4c3a0b6d89c7c4b72490410e8c255feb1a
Evelyn Fredin <evefre2001@skola.goteborg.se>
2022-03-15 19:55:06 +0100

    Better structure for tests

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,7 +2,13 @@
 
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Inertia\Testing\AssertableInertia as Assert;
+
 use function Pest\Laravel\post;
+
+it('renders the login page')
+    ->get('/login')
+    ->assertInertia(fn (Assert $page) => $page->component('Auth/Login'));
 
 it('redirects user after login', function () {
     $user = User::factory()->create();
@@ -26,3 +32,15 @@ it('prevents user from login with incorrect password', function () {
 
     $this->assertGuest();
 });
+
+it('hides login page from authenticated user', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user)
+        ->get('/login')
+        ->assertStatus(302);
+});
+
+it('displays errors when empty login details')
+    ->post('/login')
+    ->assertSessionHasErrors(['email', 'password']);
