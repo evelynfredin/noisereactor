aaf568bc02796ab8576b040f34da8c171b416a71
Evelyn Fredin <evefre2001@skola.goteborg.se>
2022-03-17 08:51:44 +0100

    Add tests

--- tests/Feature/ArtistTest.php
+++ tests/Feature/ArtistTest.php
@@ -5,6 +5,7 @@ use Inertia\Testing\AssertableInertia as Assert;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\assertDatabaseMissing;
 use function Pest\Laravel\post;
+use function Pest\Laravel\get;
 
 it('renders artists page')
     ->get('/artists')
@@ -37,4 +38,22 @@ it('prevents unauthenticated user from creating an artist', function () {
     assertDatabaseMissing('artists', [
         'name' => 'Refactoring Hell'
     ]);
+});
+
+it('renders the artist page for authenticated user', function () {
+    $user = User::factory()->create();
+
+    post('/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ]);
+
+    $this->assertAuthenticated()
+        ->get(route('artist.list'))
+        ->assertInertia(fn (Assert $page) => $page->component('Admin/ArtistList'));
+});
+
+it('protects admin artist view from unauthenticated user', function () {
+    get(route('artist.list'))
+        ->assertStatus(302);
 });
